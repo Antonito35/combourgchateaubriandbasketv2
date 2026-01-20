241603ba600b84d0d67a6a2c9eaef2243cd62cf2
Antoine SIMON <antoinesimon646@gmail.com>
2026-01-20 11:46:24 +0100

    modified:   pages/index.js 	deleted:    public/images/scenic ridwan.pdf

--- pages/index.js
+++ pages/index.js
@@ -31,6 +31,8 @@ export default function Home() {
       <Head>
         <title>Combourg Chateaubriand Basket - Club de Basketball depuis 1928</title>
         <meta name="description" content="La Chateaubriand Combourg Basket, club de basketball à Combourg (35) depuis 1928. Découvrez nos équipes, planning des entrainements, inscriptions et boutique officielle." />
+      
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       {/* Section Hero avec image de fond qui prend toute la page */}
